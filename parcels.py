d8aacd162a0f0ebce0e5aa5457a3b9a5ac6ea3d0
Paulina Grajewska <56506556+ZaulaGrajla@users.noreply.github.com>
2020-01-28 19:30:10 +0100

    Update parcels.py

--- parcels.py
+++ parcels.py
@@ -1,6 +1,4 @@
-# yield random.choice(destinations), str(datetime.now().strftime("%b %d %Y %H:%M:%S")), random.choice(sizes), serial_nr
 import random
-# from collections import namedtuple
 from datetime import datetime
 
 from dataclasses import dataclass
@@ -17,9 +15,6 @@ class Parcels:
     serial_number: int
     sender_name: str = None
     sender_origin: str = None
-
-
-# Parcel = namedtuple("Parcel", "destination send_time size serial_number")
 
 
 def __parcel_generator():
